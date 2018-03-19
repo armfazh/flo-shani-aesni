a03b55b15ddc483489e569beddb0df2c277a82ca
armfazh <armfazh@gmail.com>
2018-03-19 18:21:17 -0300

    Including bench for multiple message hahshing.

--- sha256/bench/bench.c
+++ sha256/bench/bench.c
@@ -19,80 +19,44 @@
 #include <stdint.h>
 #include <shani.h>
 #include <openssl/sha.h>
+#include<openssl/objects.h>
+#include <prng/random.h>
 #include "clocks.h"
 
-#define MAX_SIZE_BITS 21
+#define MAX_SIZE_BITS 7
 
-struct seqTimes{
+struct seqTimings {
   uint64_t size;
   uint64_t openssl;
   uint64_t shani;
 };
 
-#define BENCH_SHA_MULTI(FUNC, MSG_LEN, NUM)       \
-do{                                               \
-    int i_multi=0;                                \
-    uint8_t *message[NUM];                        \
-    uint8_t *digest[NUM];                         \
-    for(i_multi=0;i_multi<NUM;i_multi++)          \
-    {                                             \
-        message[i_multi] = (uint8_t*)_mm_malloc((MSG_LEN)+1,ALIGN_BYTES);  \
-        digest[i_multi] = (uint8_t*)_mm_malloc(32,ALIGN_BYTES);  \
-        random_bytes(message[i_multi],MSG_LEN);   \
-    }                                             \
-    CLOCKS("",FUNC(message,MSG_LEN,digest));      \
-    for(i_multi=0;i_multi<NUM;i_multi++)          \
-    {                                             \
-        _mm_free(message[i_multi]);               \
-        _mm_free(digest[i_multi]);                \
-    }                                             \
-}while(0)
+struct parallelTimings {
+  uint64_t size;
+  uint64_t _1x;
+  uint64_t _2x;
+  uint64_t _4x;
+  uint64_t _8x;
+};
 
-#define BENCH_SIZE_NW(FUNC, N)                 \
-do{                                            \
-    long BENCH = 0;                            \
-    const unsigned long MAX_SIZE_BITS=21;      \
-    unsigned long it=0;                        \
-    for(it=0;it<MAX_SIZE_BITS;it++)            \
-    {                                          \
-        int message_size = 1<<it;              \
-        BENCH = 512-it*20;                     \
-        printf("%d",message_size);             \
-        BENCH_SHA_MULTI(FUNC,message_size,N);  \
-    }                                          \
-}while(0)
-
-void mb_avx2() {
-  printf("Multibuffer SEQ/AVX/AVX2/SHANI \n");
-
-//    BENCH_SIZE_1W(SHA256);
-//  BENCH_SIZE_1W(crypto_hash_sphlib);
-//  BENCH_SIZE_NW(ntru_sha256_4way_simd, 4);
-//  BENCH_SIZE_NW(ntru_sha256_8way_simd, 8);
-  // BENCH_SIZE_NW(sha256_4w_avx,4);
-  //BENCH_SIZE_NW(sha256_8w_avx2,8);
-//  BENCH_SIZE_1W(sha256_intel);
-//  BENCH_SIZE_NW(sha256_x2_arm_x2, 2);
-//  BENCH_SIZE_NW(sha256_x4_arm_x4, 4);
-//  BENCH_SIZE_NW(sha256_x8_arm_x8, 8);
-}
-
-void print_table(struct seqTimes * table, int items)
-{
+void print_tableParallel(struct parallelTimings *table, int items) {
   int i;
-  printf("    SHA256: OpenSSL vs SHANI             \n");
-  printf("╔═════════╦═════════╦═════════╦═════════╗\n");
-  printf("║  bytes  ║ OpenSSL ║  SHANI  ║ Speedup ║\n");
-  printf("╠═════════╩═════════╩═════════╩═════════╣\n");
-  for(i=0;i<items;i++) {
-    printf("║%9ld║%9ld║%9ld║%9.2f║\n",
-           table[i].size,table[i].openssl,
-           table[i].shani,table[i].openssl/(double)table[i].shani);
+  printf(" Figure 5: Multiple-message Hashing \n");
+  printf("╔═════════╦═════════╦═════════╦═════════╦═════════╗\n");
+  printf("║  bytes  ║   1x    ║   2x    ║   4x    ║   8x    ║\n");
+  printf("╠═════════╩═════════╩═════════╩═════════╩═════════╣\n");
+  for (i = 0; i < items; i++) {
+    printf("║%9ld║%9.2f║%9.2f║%9.2f║%9.2f║\n",
+           table[i].size,
+           1.0*table[i]._1x/(double)table[i]._1x,
+           2.0*table[i]._1x/(double)table[i]._2x,
+           4.0*table[i]._1x/(double)table[i]._4x,
+           8.0*table[i]._1x/(double)table[i]._8x);
   }
-  printf("╚═════════╩═════════╩═════════╩═════════╝\n");
+  printf("╚═════════╩═════════╩═════════╩═════════╩═════════╝\n");
 }
 
-#define BENCH_SIZE_1W(FUNC,IMPL)       \
+#define BENCH_SIZE_1W(FUNC, IMPL)       \
   do {                                 \
     long BENCH_TIMES = 0, CYCLES = 0;  \
     for(it=0;it<MAX_SIZE_BITS;it++) {  \
@@ -104,27 +68,97 @@ void print_table(struct seqTimes * table, int items)
     }                                  \
   }while(0)
 
-void bench_1w(){
-  struct seqTimes table[MAX_SIZE_BITS];
-  unsigned long it=0;
-  unsigned long MAX_SIZE=1<<MAX_SIZE_BITS;
-  unsigned char * message = (unsigned char*)_mm_malloc(MAX_SIZE,ALIGN_BYTES);
+
+#define BENCH_SIZE_NW(FUNC, N)                   \
+do{                                              \
+    long BENCH_TIMES = 0, CYCLES = 0;            \
+    unsigned long it=0;                          \
+    unsigned long MAX_SIZE = 1 << MAX_SIZE_BITS; \
+    uint8_t *message[N];                       \
+    uint8_t *digest[N];                        \
+    for(it=0;it<N;it++) {                        \
+        message[it] = (uint8_t*)_mm_malloc(MAX_SIZE,ALIGN_BYTES);  \
+        digest[it] = (uint8_t*)_mm_malloc(32,ALIGN_BYTES);  \
+        random_bytes(message[it],MAX_SIZE);  \
+    }                                        \
+    for(it=0;it<MAX_SIZE_BITS;it++) {        \
+        int message_size = 1<<it;            \
+        BENCH_TIMES = 512-it*20;             \
+        CLOCKS(FUNC(message,message_size,digest));  \
+        table[it].size = message_size;   \
+        table[it]._ ## N ## x = CYCLES;  \
+    }                                    \
+    for(it=0;it<N;it++) {                \
+        _mm_free(message[it]);           \
+        _mm_free(digest[it]);            \
+    }                                    \
+}while(0)
+
+void bench_Nw() {
+  struct parallelTimings table[MAX_SIZE_BITS];
+  unsigned long it = 0;
+  unsigned char digest[32];
+  unsigned long MAX_SIZE = 1 << MAX_SIZE_BITS;
+  unsigned char *message = (unsigned char *) _mm_malloc(MAX_SIZE, ALIGN_BYTES);
+
+  printf("Multibuffer SEQ/AVX/AVX2/SHANI \n");
+
+  printf("Running 1x:\n");
+  BENCH_SIZE_1W(sha256_update_shani, _1x);
+  printf("Running 2x:\n");
+  BENCH_SIZE_NW(sha256_x2_update_shani_2x, 2);
+  printf("Running 4x:\n");
+  BENCH_SIZE_NW(sha256_x4_update_shani_4x, 4);
+  printf("Running 8x:\n");
+  BENCH_SIZE_NW(sha256_x8_update_shani_8x, 8);
+  print_tableParallel(table,MAX_SIZE_BITS);
+}
+
+
+
+void print_tableSeq(struct seqTimings *table, int items) {
+  int i;
+  printf("    SHA256: OpenSSL vs SHANI             \n");
+  printf("╔═════════╦═════════╦═════════╦═════════╗\n");
+  printf("║  bytes  ║ OpenSSL ║  SHANI  ║ Speedup ║\n");
+  printf("╠═════════╩═════════╩═════════╩═════════╣\n");
+  for (i = 0; i < items; i++) {
+    printf("║%9ld║%9ld║%9ld║%9.2f║\n",
+           table[i].size, table[i].openssl,
+           table[i].shani, table[i].openssl / (double) table[i].shani);
+  }
+  printf("╚═════════╩═════════╩═════════╩═════════╝\n");
+}
+
+void bench_1w() {
+  struct seqTimings table[MAX_SIZE_BITS];
+  unsigned long it = 0;
+  unsigned long MAX_SIZE = 1 << MAX_SIZE_BITS;
+  unsigned char *message = (unsigned char *) _mm_malloc(MAX_SIZE, ALIGN_BYTES);
   unsigned char digest[32];
 
+  uint64_t *c = OPENSSL_ia32cap_loc();
   printf("Running OpenSSL:\n");
-  BENCH_SIZE_1W(SHA256,openssl);
+  BENCH_SIZE_1W(SHA256, openssl);
+
+  // Disable SHA-NI
+  c[1] &= 0x10000000;
+  printf("Running OpenSSL:\n");
+  BENCH_SIZE_1W(SHA256, openssl);
+
   printf("Running shani:\n");
-  BENCH_SIZE_1W(sha256_update_shani,shani);
-  print_table(table,MAX_SIZE_BITS);
+  BENCH_SIZE_1W(sha256_update_shani, shani);
+  print_tableSeq(table, MAX_SIZE_BITS);
   _mm_free(message);
 }
-#undef BENCH_SIZE_1W
 
 int main(void) {
 
   printf("== Start of Benchmark ===\n");
+  printf("OpenSSL version: %s\n", SSLeay_version(SSLEAY_VERSION));
+
   bench_1w();
-  mb_avx2();
+  bench_Nw();
   printf("== End of Benchmark =====\n");
   return 0;
 }
